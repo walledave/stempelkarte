7763217567bf30bd20e569a51ef2777fc0e1da13
David Wallenborn <david.wallenborn@MSStahl.local>
2026-09-17 12:45:47 +0200

    Anvisiertes Tagesziel für Ausstempelzeit ergänzen

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service Worker: macht die Stempelkarte offline verfuegbar.
    Bei jeder neuen Version die Zahl in CACHE erhoehen. */
-const CACHE = "stempelkarte-v5";
+const CACHE = "stempelkarte-v6";
 const DATEIEN = ["./", "./index.html", "./manifest.webmanifest",
                  "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
